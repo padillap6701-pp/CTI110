e0c64c8e04fa537a5b965c48a069329837e715e2
padillap6701-pp <padillap6701@student.faytechcc.edu>
2025-09-21 21:45:14 -0400

    Add files via upload

---
+++ P1LAB1_PadillaPedro.py
@@ -0,0 +1,13 @@
+# Your Name
+# Date
+# Assignment Name: P1LAB1
+# A brief description of the project: This program takes a user's first and last name as input and prints a welcome message.
+
+# Prompt the user for their first name and store it in a variable.
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name and store it in a variable.
+last_name = input("Enter your last name: ")
+
+# Print the welcome message, incorporating the user's first and last names.
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
